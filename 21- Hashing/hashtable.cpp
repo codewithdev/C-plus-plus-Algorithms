46eca3840e592f8d36a25adfa04a56110ce8ae6d
codewithdev <dev.18715@knit.ac.in>
2020-06-28 16:52:50 +0530

    #1

--- 21- Hashing/hashtable.cpp
+++ 21- Hashing/hashtable.cpp
@@ -1,55 +1,16 @@
 #include<iostream>
+#include<cstring>
+#include "hashtable.h"
+
 using namespace std;
-
-template<typename T>
-class Node{
-public:
-	string key;
-	T value;
-	Node<T>*next;
-	Node(string key, T val){
-		this->key= key;
-		value= val;
-		next= NULL;
-	}
-	~Node(){
-		if(next!=NULL){
-			delete next;
-		}
-	}
-};
-
-template<typename T>
-class Hashtable{
-	Node<T>**table //pointer to an array of an pointers
-
-	int current_size;
-	int table_size;
-	int hashfun(string key){
-		int idx=0;
-		int p=1;
-		for(int j=0;j<key.length();j++){
-			idx= idx+(key[j]*p)%table_size;
-			idx= idx%table_size;
-			p= (p*27)%table_size;
-		}
-		return idx;
-	}
-
-public:
-	Hashtable(int ts=7){
-		table_size=ts;
-		table= new Node<T>*[table_size];
-		current_size=0;
-		for(int i=0;i<table_size;i++){
-			table[i]=NULL;
-		}
-	}
-	void insert(string key, T value){
-		int idx= hashfun(key);
-		Node<T>*n= new Node<T>(key,val);
-		n->next= table[idx];
-		table[idx]=n;
-		current_size++;
-	}
+int main(){
+ hashtable<int>price_menu;
+ price_menu.insert("Burger",100);
+ price_menu.insert("Pizza",120);
+ price_menu.insert("Potato",160);
+ price_menu.insert("IceCream",130);
+ price_menu.insert("Coldrink",190);
+ price_menu.insert("Samosa",10)
+ price_menu.print();
+ return 0;
 }
