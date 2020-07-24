fdac6a0707139eac184a305797f5695f15d89dd4
codewithdev <dev.18715@knit.ac.in>
2020-07-24 14:47:28 +0530

    Updated

---
+++ 23- Greedy Algorithms/BiasedStanding.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	int arr[100000]= {0};
+	int t,n;
+	cin>>t;
+	while(t--){
+		memset(arr,0,sizeof arr);
+		string name;
+		cin>>n;
+		int rank;
+		for(int i=0;i<n;i++){
+             cin>>name>>rank;
+		     arr[rank]++;
+		}
+		int actual_rank=1;
+		int sum=0;
+		for(int i=1;i<=n;i++){
+			while(arr[i]){
+				sum+=abs(actual_rank-i);
+				arr[i]--;
+				actual_rank++;
+			}
+		}
+		cout<<sum<<endl;
+	}
+}
---
+++ 23- Greedy Algorithms/connectWires.cpp
@@ -0,0 +1,36 @@
+/*A Wire Connections game board consists of a list of n white dots and n blacks dots, interleaved in
+ an arbitrary order. For example:
+
+    W W B W B B
+
+Input
+
+2
+4 4
+1 2 8 6
+2 3 4 9
+
+output
+
+1 2 2 3 6 4 8 9
+
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,n,m;
+	cin>>t;
+	while(t--){
+		cin>>m>>n;
+		int *arr1= new int[m];
+		int *arr2= new int[n];
+		for(int i=0;i<m;i++){
+			cin>>arr[i];
+		}
+		fo
+	}
+}
