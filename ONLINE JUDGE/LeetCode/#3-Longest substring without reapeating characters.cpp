1c34087a938738c6598bf597476e10e9467efa74
Priyanshi Sharma <priyanshi417@gmail.com>
2020-10-04 03:36:34 +0530

    Create #3-Longest substring without reapeating characters.cpp

---
+++ ONLINE JUDGE/LeetCode/#3-Longest substring without reapeating characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      map<char, int> Mp;
+        int start = -1;
+        int len = 0;
+        
+        for(int i=0; i< s.size(); i++){
+            if(Mp.count(s[i]) != 0)
+            {
+                start = max(start, Mp[s[i]]);
+            }
+            Mp[s[i]] = i;
+            len = max(len, i-start);
+            cout<<start<<' '<<Mp[s[i]]<<' '<<len<<endl;
+        }
+        return len;
+    }
+};
