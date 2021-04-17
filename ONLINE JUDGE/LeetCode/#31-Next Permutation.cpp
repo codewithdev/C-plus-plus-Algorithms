9c326ce961c7f7bb2fdbbd7652c6b467cdfcbeac
Dev Prakash Sharma <44609609+codewithdev@users.noreply.github.com>
2021-04-17 14:24:59 +0530

    Create #31-Next Permutation.cpp

---
+++ ONLINE JUDGE/LeetCode/#31-Next Permutation.cpp
@@ -0,0 +1,24 @@
+#Problem Link: https://leetcode.com/problems/next-permutation/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n= nums.size();
+        int k,l;
+        for(k=n-2;k>=0;--k){
+            if(nums[k]<nums[k+1])
+                break;
+        }
+             if(k<0)
+             {
+                 reverse(nums.begin(),nums.end());
+             }else{
+                 for(l=n-1;l>k;--l){
+                     if(nums[l]>nums[k])
+                         break;
+                 }
+                 swap(nums[k],nums[l]);
+                 reverse(nums.begin()+k+1, nums.end());
+             }
+        }
+};
