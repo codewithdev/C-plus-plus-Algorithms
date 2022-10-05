5366cb1188a69bfe7ff1edfc6459b145a0d300fd
Abhijeet Tiwari <abhijeet1862@gmail.com>
2022-10-06 02:10:42 +0530

    changes made

--- 24- Dynamic Programming/ninjaAndHisFriends.cpp
+++ 24- Dynamic Programming/ninjaAndHisFriends.cpp
@@ -1,14 +1,22 @@
-// Problem(3-D DP) : We are given an ‘N *M’ matrix.Every cell of the matrix has some chocolates on it, mat[i][j] gives us the
-// number of chocolates.We have two friends ‘Alice’ and ‘Bob’.initially, Alice is standing on the cell(0, 0) and Bob is standing
-// on the cell(0, M - 1).Both of them can move only to the cells below them in these three directions : to the bottom cell(↓),
-// to the bottom - right cell(↘), or to the bottom - left cell(↙). When Alica and Bob visit a cell, they take all the chocolates
-// from that cell with them. It can happen that they visit the same cell, in that case, the chocolates need to be considered only
-// once. They cannot go out of the boundary of the given matrix, we need to return the maximum number of chocolates that Bob and
-// Alice can together collect.
+// Problem(3-D DP) : https://www.codingninjas.com/codestudio/problems/ninja-and-his-friends_3125885
+
+// Sample Inputs
+// 3 4
+// 2 3 1 2
+// 3 4 2 2
+// 5 6 3 5
+// 2 2
+// 1 1
+// 1 2
+
+// Corresponding Outputs
+// 21
+// 5
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// recursive function
 int f(int i, int j1, int j2, vector<vector<int>> &grid, int n, int m, vector<vector<vector<int>>> &dp)
 {
     if (i == n)
@@ -76,6 +84,7 @@ int f(int i, int j1, int j2, vector<vector<int>> &grid, int n, int m, vector<vec
     }
 }
 
+// function returning answer
 int maximumChocolates(int r, int c, vector<vector<int>> &grid)
 {
     vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c)));
@@ -106,20 +115,3 @@ int main()
     }
     cout << maximumChocolates(r, c, grid);
 }
-
-// Sample Inputs
-
-// 3 4
-// 2 3 1 2
-// 3 4 2 2
-// 5 6 3 5
-
-// 2 2
-// 1 1
-// 1 2
-
-// Corresponding Outputs
-
-// 21
-
-// 5
