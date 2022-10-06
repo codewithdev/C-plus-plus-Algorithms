984c1addc8dfea4712ae60a5e199d4fc471c40fa
Dev Prakash Sharma <idevprakaash@gmail.com>
2022-10-06 19:49:39 +0530

    Update ninjaAndHisFriends.cpp

--- 24- Dynamic Programming/ninjaAndHisFriends.cpp
+++ 24- Dynamic Programming/ninjaAndHisFriends.cpp
@@ -1,17 +1,20 @@
-// Problem(3-D DP) : https://www.codingninjas.com/codestudio/problems/ninja-and-his-friends_3125885
+// Problem(3-D DP) :
 
-// Sample Inputs
-// 3 4
-// 2 3 1 2
-// 3 4 2 2
-// 5 6 3 5
-// 2 2
-// 1 1
-// 1 2
+// We are given an ‘N *M’ matrix.Every cell of the matrix has some chocolates on it, mat[i][j] gives us the number of chocolates.We have two friends ‘Alice’ and ‘Bob’.initially, Alice is standing on the cell(0, 0) and Bob is standing on the cell(0, M - 1).Both of them can move only to the cells below them in these three directions: to the bottom cell(↓), to the bottom-right cell(↘), or to the bottom-left cell(↙). // When Alica and Bob visit a cell, they take all the chocolates from that cell with them. It can happen that they visit the same cell, in that case, the chocolates need to be considered only once. They cannot go out of the boundary of the given matrix, we need to return the maximum number of chocolates that Bob and Alice can together collect.
 
-// Corresponding Outputs
-// 21
-// 5
+/* Sample Inputs
+   3 4
+   2 3 1 2
+   3 4 2 2
+   5 6 3 5
+   2 2
+   1 1
+   1 2
+
+Corresponding Outputs
+   21
+   5
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
