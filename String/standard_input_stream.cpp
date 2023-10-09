5434a5c3481b365f229bfb5a65d2d942e89e7080
stevenanthonyrevo <stevenanthonyrevo@live.com>
2023-10-08 20:58:37 -0400

    Added Multi-line Comments and Single Line Comments

--- String/standard_input_stream.cpp
+++ String/standard_input_stream.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+/* The following code is an example which use cin to write the captured input via the terminal to the input stream 
+   and then displays the captured input via cout to the output stream. 
+   
+   link: https://en.cppreference.com/w/cpp/header/iostream  
+*/
+
 int main() {
   int price;
+
+  // Writes to the standard C output stream 
   std::cout << "Enter the price: ";
+  
+  // Reads from the standard C input stream 
   std::cin >> price;
+
   std::cout << "User entered the price " << price << std::endl;
 }
